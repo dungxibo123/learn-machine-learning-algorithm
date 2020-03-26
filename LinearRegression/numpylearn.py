0b4daad63c43b6e80527e9a309eca7263c944407
dungxibo123 <votiendung0805@gmail.com>
2020-03-26 19:49:30 +0700

    error in linear regression

--- LinearRegression/numpylearn.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-_A = [[1,2,3],[4,5,2],[66,7,8]]
-A = np.array([[1,2,3],[1,23,0]])
-
-sum = np.sum(A)
-print(sum)
---
+++ LinearRegression/test.py
@@ -0,0 +1,16 @@
+import numpy as np
+from Fundamental import *
+
+A = np.loadtxt('da.txt',delimiter = ',')
+
+X = A[:,:2]
+Y = A[:,2:]
+needPredict = np.array([1,2032,7])
+# X = np.append([[1]]*np.size(X,0),X,1) #check
+# m = np.size(X,0) #check
+# n = np.size(X,1)
+
+alpha = 0.003
+# theta = GradientDescent(X,Y,alpha = 0.03, iter = 500)
+# print(theta)
+[theta,J_hist] = GradientDescent(X,Y,alpha)
